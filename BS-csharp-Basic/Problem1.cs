2ab0e20434794ebd4557611b1112a5ae8d030cd1
BS1207 <refatm452@gmail.com>
2022-08-09 12:38:08 +0600

    solution-4 added

--- BS-csharp-Basic/Problem1.cs
+++ BS-csharp-Basic/Problem1.cs
@@ -31,7 +31,40 @@ namespace BS_csharp_Basic
             catch (OverflowException ex)
             {
                 Console.WriteLine(ex.Message);
-            }           
+            }
+
+            //problem-4
+            string str1 = "123";
+            int num1=Int32.Parse(str1);
+
+            string str2 = "10000000000";
+            long num2=Int64.Parse(str2);
+
+            string str3 = "123.50";
+            double num3=Double.Parse(str3);
+
+            int num4 = 23;
+            string str4 = num4.ToString();
+
+            string str5 = "true";
+            bool flag = bool.Parse(str5);
+
+            bool flag1=true;
+            string str6 = flag1.ToString();
+
+            int num5 = 100;
+            long num6 =num5;
+
+            char ch1 = 'A';
+            int num7 = ch1;
+
+            int num8 = 25;
+            char ch2 = (char)(num8 + 'A'); 
+
+
+            Console.WriteLine($"string to int {num1}\nstring to long {num2}\nstring to double {num3}\n" +
+                $"int to string {str4}\nstring to bool {flag}\nchar to ASCII {num7}\n"+
+                $"ASCII to char {ch2}");
             Console.ReadKey();
         }
     }
