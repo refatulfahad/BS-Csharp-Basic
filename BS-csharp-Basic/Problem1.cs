02e6b0511c777bac3ef55d8d26b1b2e45fd2dd37
BS1207 <refatm452@gmail.com>
2022-08-09 10:40:44 +0600

    added new project

---
+++ BS-csharp-Basic/Problem1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS_csharp_Basic
+{
+    internal class Problem1
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
